330e6a1ea4725312012f7c37f6a534a207e320bf
9122019 <67593511+9122019@users.noreply.github.com>
2020-06-29 22:21:56 +0700

    Update send.php

--- send.php
+++ send.php
@@ -1,17 +1,19 @@
 <?php
 
 $post = [
-	    'tel' => '+71234567890',
-	    'city' => 'Нижний Новгород',
-	    'question' => 'Как получить жилье от государства?'
+	'self.key' => '12345',
+	'self.name' => 'Иванов Иван Иванович',
+	'self.phone' => '+71234567890',	// в любом формате
+	'self.city' => 'Нижний Новгород',
+	'self.region' => 'Нижегородская область',
+	'self.question' => 'Как получить жилье от государства?'
 ];
 
 $url = 'https://lawtask.ru/wh.php';
 	
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_close ($ch);
