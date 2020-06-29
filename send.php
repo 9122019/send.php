9b80ecff919f84ea5b316e0a46491325892ebdd7
9122019 <67593511+9122019@users.noreply.github.com>
2020-06-29 21:41:34 +0700

    Пример файла отправки

---
+++ send.php
@@ -0,0 +1,15 @@
+$post = [
+	    'tel' => '+71234567890',
+	    'city' => 'Нижний Новгород',
+	    'question' => 'Как получить жилье от государства?'
+];
+
+$url = 'https://lawtask.ru/wh.php';
+	
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_close ($ch);
