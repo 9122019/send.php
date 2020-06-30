52b126b8b35130e16d8b3fa225bfc1297ccd30d7
9122019 <67593511+9122019@users.noreply.github.com>
2020-06-30 23:14:33 +0700

    Update send.php

--- send.php
+++ send.php
@@ -1,13 +1,23 @@
 <?php
 
+// собираем
+
 $post = [
-	'self.key' => '12345',
-	'self.name' => 'Иванов Иван Иванович',
-	'self.phone' => '+71234567890',	// в любом формате
-	'self.city' => 'Нижний Новгород',
-	'self.region' => 'Нижегородская область',
-	'self.question' => 'Как получить жилье от государства?'
+	'key' => '12345',
+	'name' => 'Иванов Иван Иванович',
+	'phone' => '+71234567890',	// в любом формате
+	'city' => 'Нижний Новгород',
+	'region' => 'Нижегородская область',
+	'question' => 'Как получить жилье от государства?'
 ];
+
+// очищаем
+
+foreach ($post as $key => $value) {
+	$post[$key] = htmlspecialchars($value);
+}
+
+// отправляем
 
 $url = 'https://lawtask.ru/wh.php';
 	
@@ -16,4 +26,15 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
 curl_close ($ch);
+
+// проверяем
+
+if ($response != true) {
+	error_log('Лид не был сохранён получателем');
+	echo 'error';
+	var_dump($response);
+} else {
+	echo 'Лид был успешно передан и сохранен получатаем';
+}
